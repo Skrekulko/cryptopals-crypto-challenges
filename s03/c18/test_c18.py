c157e90915f19c4756509226c5488eb05abd4c82
221565 <rybar.mtj@gmail.com>
2022-09-26 11:11:29 +0200

    feat: c18 solution

--- s03/c18/test_c18.py
+++ s03/c18/test_c18.py
@@ -5,9 +5,10 @@
 from c18 import c18
 
 def test_c18() -> None:
-    input_plaintext = b"L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=="
-    input_encrypted = b""
+    input = b"L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=="
+    key = b"YELLOW SUBMARINE"
+    nonce = 0
     
-    assert c18()["encrypted"] == input_encrypted
+    encrypted = c18(input, key, nonce)
     
-    assert c18()["decrypted"] == input_plaintext
+    assert c18(encrypted, key, nonce) == input
