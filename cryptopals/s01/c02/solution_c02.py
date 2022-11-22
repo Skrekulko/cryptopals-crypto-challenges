c03597b901bcbb554a64d0afae3f5cf71e3568b0
221565 <rybar.mtj@gmail.com>
2022-11-22 09:37:46 +0100

    refac: remove class

--- cryptopals/s01/c02/solution_c02.py
+++ cryptopals/s01/c02/solution_c02.py
@@ -2,10 +2,8 @@
 #   02 - Fixed XOR
 #
 
-class XOR:
-    @staticmethod
-    def fixed_xor(byte_string1: bytes, byte_string2: bytes) -> bytes:
-        if len(byte_string1) == len(byte_string2):
-            return bytes(a ^ b for (a, b) in zip(byte_string1, byte_string2))
-        else:
-            raise ValueError
+def fixed_xor(byte_string1: bytes, byte_string2: bytes) -> bytes:
+    if len(byte_string1) == len(byte_string2):
+        return bytes(a ^ b for (a, b) in zip(byte_string1, byte_string2))
+    else:
+        raise ValueError
