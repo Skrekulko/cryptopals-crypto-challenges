7d116bc2b225c7d08f2d567c3daead31a47faebf
Skrekulko <rybar.mtj@gmail.com>
2023-02-07 17:27:18 +0100

    refac: move attack implementation to solution

--- cryptopals/s06/c46/solution_c46.py
+++ cryptopals/s06/c46/solution_c46.py
@@ -3,6 +3,8 @@
 #
 
 from cryptopals.asymmetric import RSA
+from math import ceil, log
+from decimal import Decimal, getcontext
 
 
 class Oracle(RSA):
@@ -15,3 +17,30 @@ class Oracle(RSA):
             return True
 
         return False
+
+
+def rsa_parity_attack(oracle: Oracle, ciphertext: int) -> int:
+    # Compute The Ciphertext Of 2, Which Is Used As The Ciphertext Multiplier
+    multiplier = pow(2, oracle.parameters.e, oracle.parameters.n)
+
+    # Initialize The Lower And The Upper Bound, And Use Decimal To Allow Better Precision
+    lower_bound = Decimal(0)
+    upper_bound = Decimal(oracle.parameters.n)
+
+    # Number Of Iterations Needed
+    k = int(ceil(log(oracle.parameters.n, 2)))
+
+    # Precision Floating Point Number
+    getcontext().prec = k
+
+    # Binary Search
+    for _ in range(k):
+        ciphertext = (ciphertext * multiplier) % oracle.parameters.n
+
+        if oracle.is_parity_odd(ciphertext):
+            lower_bound = (lower_bound + upper_bound) / 2
+        else:
+            upper_bound = (lower_bound + upper_bound) / 2
+
+    # Return The Binary Version Of The Upper bound
+    return int(upper_bound)
--- cryptopals/s06/c46/test_c46.py
+++ cryptopals/s06/c46/test_c46.py
@@ -2,37 +2,8 @@
 #   46 - RSA parity oracle
 #
 
-from cryptopals.s06.c46.solution_c46 import Oracle
+from cryptopals.s06.c46.solution_c46 import Oracle, rsa_parity_attack
 from cryptopals.utils import Converter
-from math import ceil, log
-from decimal import Decimal, getcontext
-
-
-def rsa_parity_attack(oracle: Oracle, ciphertext: int) -> int:
-    # Compute The Ciphertext Of 2, Which Is Used As The Ciphertext Multiplier
-    multiplier = pow(2, oracle.parameters.e, oracle.parameters.n)
-
-    # Initialize The Lower And The Upper Bound, And Use Decimal To Allow Better Precision
-    lower_bound = Decimal(0)
-    upper_bound = Decimal(oracle.parameters.n)
-
-    # Number Of Iterations Needed
-    k = int(ceil(log(oracle.parameters.n, 2)))
-
-    # Precision Floating Point Number
-    getcontext().prec = k
-
-    # Binary Search
-    for _ in range(k):
-        ciphertext = (ciphertext * multiplier) % oracle.parameters.n
-
-        if oracle.is_parity_odd(ciphertext):
-            lower_bound = (lower_bound + upper_bound) / 2
-        else:
-            upper_bound = (lower_bound + upper_bound) / 2
-
-    # Return The Binary Version Of The Upper bound
-    return int(upper_bound)
 
 
 def test_c46() -> None:
